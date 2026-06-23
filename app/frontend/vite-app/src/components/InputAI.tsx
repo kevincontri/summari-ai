e41d9db3af1bc7fc0b2a11fd8fe7130a3968350d
kevincontri <kevin.lucas.contri@gmail.com>
2026-06-22 23:09:14 -0300

    Initialized AI output container

--- app/frontend/vite-app/src/components/InputAI.tsx
+++ app/frontend/vite-app/src/components/InputAI.tsx
@@ -1,7 +1,9 @@
 import { Input } from "../components/ui/input"
+import { X } from 'lucide-react';
 
 export default function InputAI({ handleSubmit, aiQuery, setAIQuery }: { handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void; aiQuery: string; setAIQuery: (query: string) => void }) {
   return (
+    <>
     <div className="ai-input-container">
       <div className="flex flex-row items-center space-x-2 w-full">
         <div className="ai-icon-container hover:animate-spin">
@@ -24,5 +26,30 @@ export default function InputAI({ handleSubmit, aiQuery, setAIQuery }: { handleS
         </div>
       </div>
     </div>
+    {/* Output Modal */}
+    <div className="ai-output-container">
+      <div className="ai-output-header">
+        <div className="flex flex-row items-center space-x-2">
+          <img src="./src/assets/stars.png" alt="Stars Icon" title="Summari AI" className="ai-icon" />
+          <h2 className="ai-output-title">Summari</h2>
+        </div>
+        <X onClick={() => setAIQuery("")} className="cursor-pointer"/>
+      </div>
+      {/* TODO: MAKE THIS DYNAMIC + COMPONENT DEDICATED FOR THIS */}
+      <div className="ai-output-content">
+        <p className="ai-output-text">Here's what I found across your notes: "Embeddings vs. keyword search: what actually wins" looks most relevant — Cosine similarity holds up surprisingly well on short notes.</p>
+      </div>
+      <div className="flex flex-col md:flex-row items-center md:space-x-2">
+        <div className="note-opener-container">
+          <img src="./src/assets/dot.png" alt="Dot Icon" className="cursor-pointer h-2.5" />
+          <span className="note-opener">Embeddings vs. keyword search: what actually wins</span>
+        </div>
+        <div className="note-opener-container">
+          <img src="./src/assets/dot.png" alt="Dot Icon" className="cursor-pointer h-2.5" />
+          <span className="note-opener">Embeddings vs. keyword search: what actually wins</span>
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
